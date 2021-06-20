cd25dfd70875e37046c26e1bfd610c6548289f0d
Fede Wagner <83042144+polaco22@users.noreply.github.com>
2021-06-20 15:56:08 -0300

    Acomodando head, header y footer en ProductDetail y Cart

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 const mainController = {
-    home: (req,res) => { res.render("home")},
+    home: (req,res) => { res.render('home')},
     register: (req,res) => { res.render(('register'))},
     login: (req,res) => { res.render(('login'))},
     product: (req,res) => { res.sendFile(path.resolve(__dirname,'../views/product.html'))},
-    productCart: (req,res) => { res.sendFile(path.resolve(__dirname,'../views/productCart.html'))},
-    productDetail: (req,res) => { res.sendFile(path.resolve(__dirname,'../views/productDetail.html'))},
+    productCart: (req,res) => { res.render('productCart')},
+    productDetail: (req,res) => { res.render('productDetail')},
 }
 
 module.exports = mainController;
