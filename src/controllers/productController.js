4882127229c84a73ee8c72f167ef5fb5c375c4b3
Fede Wagner <83042144+polaco22@users.noreply.github.com>
2021-07-07 20:50:13 -0300

    Te como te como te comoooo

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -26,7 +26,7 @@ const productController = {
         let result = product.delete(req.params.id);
         return result == true ? res.redirect ('/product'):res.send('No eliminaste nada'); 
     },
-    productCart: (req,res) => { res.render('productCart')},
+    productCart: (req,res) => { res.send('Carrito en ensamble')},
 }
 
 module.exports = productController;
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -6,10 +6,7 @@ const multer = require('multer');
 const path = require('path');
 let dest = multer.diskStorage({
     destination: function (req, file, cb) {
-        let extension = path.extname(file.originalname);
-        if(extension.indexOf("jpg") > 0){
             cb(null, path.resolve(__dirname,"../../public/uploads","products"))
-        }
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
@@ -23,7 +20,7 @@ const productController = require('../controllers/productController');
 // Seteando la ruta de vista
 
 router.get('/product', productController.show); // listado de productos
-router.get('/product/cart', productController.productCart); // 8 - CARRITO
+router.get('/product/cart/:id', productController.productCart); // 8 - CARRITO
 router.get('/product/detail/:id', productController.productDetail); // detalle de un producto particular ??
 router.get('/product/create', productController.productCreate); // formulario creación
 router.post('/product', upload.single('image'), productController.store); // acción de creación (a donde se envía el formulario)
