4ae15e8a8200499410d70ffb8d7511a6c48bc077
jolazabal87 <ignacioolazabal@hotmail.com>
2021-07-19 23:17:07 -0300

    incluyendo enctype + ajustando listado provincias

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,8 +7,8 @@ const userController = {
     register: (req,res) => { res.render('register')},
     login: (req,res) => { res.render('login')},
     save: (req,res) => {
-        let result = user.create(req.body,req.file);
-        return result == true ? res.redirect ('/'):res.send('No cargaste nada');}
+        let result = user.create(req.body, req.file);
+        return result == true ? res.redirect ('/') : res.send('No cargaste nada');}
 }
 
 module.exports = userController;
