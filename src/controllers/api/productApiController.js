9daae6c78a0826fba3c3ee2901d504fdc874d71d
jolazabal87 <ignacioolazabal@hotmail.com>
2021-09-20 10:50:10 -0300

    haciendo ajustes

--- src/controllers/api/productApiController.js
+++ src/controllers/api/productApiController.js
@@ -6,6 +6,23 @@ const { Op, where, NUMBER } = require("sequelize");
 const productApiController = {
     index: async (req,res) => { 
          try {
+            const products = await db.Product.findAll({include: [ "imagenes", "colors", "categories" ]});
+            return res.status(200).json({
+                count: products.length,
+                countByCategory: {
+                    Casual: products.filter(category => category.categories.name == "Casual").length,
+                    Aventura: products.filter(category => category.categories.name == "Aventura").length,
+                },
+                data: products,
+                url: '/api/products',
+                status: 200
+            })}
+
+        catch (err) { 
+            console.log(err)}
+        },
+    page: async (req,res) => { 
+        try {
             //let page = Number.parseInt(req.query.page);
             let size = Number.parseInt(req.query.size);
 
